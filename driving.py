da50c3d889f51a41943c02245ff281f65b6a1ac9
vila74223 <vilahe74223@gmail.com>
2020-08-28 13:00:23 +0900

    add US part

--- driving.py
+++ driving.py
@@ -3,6 +3,13 @@ age = input('Please enter your age: ')
 age = int(age)
 if country == 'China':
 	if age >= 18:
-		print('You can apply for driving license.')
+		print('You can apply for the driving license.')
 	else:
 		print('You have not reached the age.')
+elif country == 'US':
+	if age >= 16:
+		print('You can apply for the driving license.')
+	else:
+		print('You have not reached the age.')
+else:
+	print('Please enter China or US' )
